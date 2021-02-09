e4b8f54a2e9bfe1526df51550424c42d0cf66b23
Anthony Delosa <adelosa@gmail.com>
2021-02-09 21:24:05 +1000

    Build the z390 Jar file

--- .github/actions/buildjar/entrypoint.sh
+++ .github/actions/buildjar/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -l
 echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+javaversion=$(java -version)
+echo "::set-output name=javaversion::$javaversion"
+# Create build directory
+mkdir build
+# Compile MessageBox first, otherwise compile fails
+javac -d ./build ./src/MessageBox.java -g:none -encoding Windows-1252
+# Compile remaining modules
+javac -d ./build --source-path ./src/*.java -g:none -encoding Windows-1252 -cp ./build
+# Create the Jar
+jar cmf ./src/z390.man ./build/z390.jar ./build/*.class
