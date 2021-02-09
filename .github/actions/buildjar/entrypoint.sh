0c8487ba922ec856d7c02471d3e6c2b4e8d5658d
Anthony Delosa <adelosa@gmail.com>
2021-02-09 21:35:16 +1000

    Vebose logging for build, fix MAN filename

--- .github/actions/buildjar/entrypoint.sh
+++ .github/actions/buildjar/entrypoint.sh
@@ -5,8 +5,8 @@ echo "::set-output name=javaversion::$javaversion"
 # Create build directory
 mkdir build
 # Compile MessageBox first, otherwise compile fails
-javac -d ./build ./src/MessageBox.java -g:none -encoding Windows-1252
+javac -d ./build ./src/MessageBox.java -g:none -encoding Windows-1252 -verbose
 # Compile remaining modules
-javac -d ./build --source-path ./src/*.java -g:none -encoding Windows-1252 -cp ./build
+javac -d ./build --source-path ./src/*.java -g:none -encoding Windows-1252 -cp ./build -verbose
 # Create the Jar
-jar cmf ./src/z390.man ./build/z390.jar ./build/*.class
+jar cvmf ./src/Z390.MAN ./build/z390.jar ./build/*.class
