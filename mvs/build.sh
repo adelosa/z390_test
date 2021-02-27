a3b6b39adddcfdbe7fa6c07e00e3e71d27470d5f
Anthony Delosa <adelosa@gmail.com>
2021-02-27 21:52:30 +1000

    Add build for mvs

---
+++ mvs/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+source ../build.sh 
+setoptions "${basedir}/build/mvs"
+options+=" sysmac(${basedir}/mvs/maclib+${basedir}/mac)"
+
+# clear the generated files
+clean
+asmlg demo/DEMO @${basedir}/RT/RT1
+asml  test/TESTMVS1 bal notiming stats objhex
+asml  test/TESTCVTX bal notiming stats objhex
+asml  test/TESTCVT1 bal notiming stats objhex
